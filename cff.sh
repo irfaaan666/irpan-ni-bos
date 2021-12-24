1d8b2bbcbfdf4b44d2cfade705c564b2f3d4dbdf
irfaaan666 <91285365+irfaaan666@users.noreply.github.com>
2021-12-24 16:59:32 +0700

    Add files via upload

--- cff.sh
+++ cff.sh
@@ -3,7 +3,7 @@
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-MYIP=$(wget -qO- icanhazip.com);
+MYIP=$(wget -qO- ifconfig.co);
 echo "Checking VPS"
 IZIN=$( curl http://akses-irfan.ueuo.com/ip | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
